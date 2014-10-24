b5d52ef9179e7c652b594cde5de310aed31835a0
Seth Malaki <seth.malaki@gmail.com>
2014-10-24 18:20:31 +0800

    Connect vs Express compatibility issues, and Fiber wrap.
    - `prerender-node` expects req.get() which just returns headers. 
    - Wrap in fiber so it won't screw with current request headers.

--- dfischer:prerenderio.js
+++ dfischer:prerenderio.js
@@ -1,8 +1,19 @@
 // Write your package code here!
 console.info('Prerender Token:',Meteor.settings.PrerenderIO.token);
+
 Meteor.startup(function() {
+  var Fiber = Npm.require('fibers');
   var prerenderio = Npm.require('prerender-node').set('prerenderToken', Meteor.settings.PrerenderIO.token);
 
-  // Feed it to middleware! (app.use)
-  WebApp.connectHandlers.use(prerenderio);
+  Fiber(function() { // run in a fiber so it won't screw with the current request object
+		WebApp.rawConnectHandlers.use(function(req, res, next) { // rawConnectHandlers so it loads before any default handlers.
+		        // req.get - quick compatibility fix for connect req object to express req object
+		        req.get = function(param) {
+		          return req.headers[param.toLowerCase()];
+		        };
+            // send request to prerender.io
+		        prerenderio(req, res, next);
+		        return next();
+		});
+	});
 });
