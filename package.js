b96ebdd8a1a252ce27b8ead15c49596bdc2b5219
DFischer <me@danielfischer.com>
2016-02-16 20:53:07 -0800

    update version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dfischer:prerenderio',
   summary: "Node wrapper for prerenderio",
-  version: "1.0.6",
+  version: "1.0.7",
   git: "https://github.com/dfischer/meteor-prerenderio"
 });
 
